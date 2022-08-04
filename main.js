cbfcd5111047939ad6c81dfde04267379a4692b4
Serge Paskal <3990195+srgpsk@users.noreply.github.com>
2022-08-04 11:11:58 -0500

    fix: timings

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-const textContainer = document.querySelector('[data-text-container]');
 const animationContainer = document.querySelector('[data-animation-container]');
 
-const animationTypes = ['animationstart', 'animationend', 'animationcancel', 'animationiteration'];
-const animationHandler = a => {
-    const suffix = a.type.substring(9).replace(/^\w/, ch => ch.toUpperCase()); // Start, End etc.
-    const listenerName = a.animationName
-        .replaceAll(/-\w/g, m => m.replace('-', '').toUpperCase()) // CSS animation name to camel Case
-        .concat(suffix);
-
-    animationListeners[listenerName]?.call(window, a);
-}
-animationTypes.forEach(t => animationContainer.addEventListener(t, animationHandler, false));
-
+// animations
 const animationListeners = {};
-const registerAnimationListener = (listenerName, callback) => animationListeners[listenerName] = callback;
+
+const animationListener = () => {
+    const animationHandler = a => {
+        const suffix = a.type.substring(9).replace(/^\w/, ch => ch.toUpperCase()); // Start, End etc.
+        const listenerName = a.animationName
+            .replace(/-\w/g, m => m.replace('-', '').toUpperCase()) // CSS animation name to camel Case
+            .concat(suffix);
+
+        animationListeners[listenerName]?.call(window, a);
+    }
+    const animationTypes = ['animationstart', 'animationend', 'animationcancel', 'animationiteration'];
+    animationTypes.forEach(t => document.addEventListener(t, animationHandler, false));
+
+    return (listenerName, callback) => animationListeners[listenerName] = callback;
+}
+const addAnimationListener = animationListener();
+
+addAnimationListener('igogoStart', () => play('igogo'));
+addAnimationListener('runningAwayToTheSunsetStart', () => play('skok'));
+addAnimationListener('runningAwayToTheSunsetEnd', () => pause('skok'));
+
+// sounds
 const soundHandler = (soundName, action) => {
     const mediaElement = document.querySelector(`[data-sound-${soundName}]`);
     (new Audio())[action].call(mediaElement);
@@ -23,41 +33,57 @@ const curry = action => soundName => soundHandler(soundName, action);
 const play = curry('play');
 const pause = curry('pause');
 
-registerAnimationListener('igogoStart', () => play('igogo'));
-registerAnimationListener('runningAwayToTheSunsetStart', () => play('skok'));
-registerAnimationListener('runningAwayToTheSunsetEnd', () => pause('skok'));
+// start action
+const actionize = selector => selector.classList.add('action');
 
-document.addEventListener('click', e => {
-    if (undefined === e.target.dataset.action) return;
+/**
+ * Animate main text block
+ */
+const animateTextContainer = r => {
+    const textContainer = document.querySelector('[data-text-container]');
+    textContainer.addEventListener('transitionend', e => r());
 
-    // change text container
-    textContainer.classList.add('action');
-    document.querySelector('[data-sound-magic]').play();
+    actionize(textContainer);
+    play('magic');
+}
 
-    // stop the cloud and add the paper animation
-    const cloudContainer = document.querySelector('[data-cloud-container]');
-    cloudContainer.style.animationPlayState = 'paused';
-    cloudContainer.classList.add('action');
-
+const animateMainScene = r => {
     // start main scene action
     animationContainer.classList.add('action');
+    r();
+}
 
+/**
+ * Stop the cloud and add the paper animation
+ */
+const animateCloud = r => {
+    const cloudContainer = document.querySelector('[data-cloud-container]');
+    cloudContainer.style.animationPlayState = 'paused';
+    actionize(cloudContainer);
+    r();
+}
+
+const animateRainbows = r => {
     // unicorn stops shitting
     document.getAnimations().forEach(a => {
         if ('rainbow' !== a.animationName) return;
 
-        console.log(a)
         a.effect.updateTiming({iterations: 1});
         a.finish();
         a.cancel();
     })
     document.querySelectorAll('.rainbow').forEach(el => {
         el.classList.add('action');
-
-        // remove rainbows from unicorn container
-        animationContainer.firstElementChild.appendChild(el);
     });
 
+   document.querySelector('.action.rainbow').ontransitionend = e => {
+       animationContainer.firstElementChild.appendChild(e.target)
+       r()
+   }
+
+}
+
+const animateSunset = r => {
     // split the sun
     const sun = document.querySelector('[data-sun-raised]');
     const sunTopHalf = document.querySelector('[data-sun-top-half]');
@@ -68,16 +94,34 @@ document.addEventListener('click', e => {
         sun.classList.add('d-none');
 
         setTimeout(() => {
-            document.querySelector('[data-sound-knife]').play();
+            play('knife');
             sunTopHalf.classList.replace('d-none', 'action');
             sunBottomHalf.classList.replace('d-none', 'action');
+            r()
         }, 1500);
     }
+}
 
+const animateUnicorn = r => {
     // unicorn igogo and run
     const unicornContainer = document.querySelector('[data-unicorn-container]');
     unicornContainer.classList.add('action');
+    r()
+}
 
+const run = async f => new Promise(resolve => f(resolve))
+const runAll = async () => {
+    await run(animateTextContainer)
+    await run(animateCloud)
+    await run(animateMainScene)
+    await run(animateRainbows)
+    await run(animateSunset)
+    await run(animateUnicorn)
+}
+document.addEventListener('click', e => {
+    if (undefined === e.target.dataset.action) return;
+
+    runAll().then(() => window.location.replace('#'))
 }, false);
 
 
